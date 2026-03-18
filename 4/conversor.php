9a94daf417166647545b6c68f1bcdc9a26dceff5
Eduardo <eduardomorgado58@gmail.com>
2026-03-18 15:54:47 -0300

    ex4

---
+++ 4/conversor.php
@@ -0,0 +1,8 @@
+ <?php
+    $c = $_POST['c'];
+
+    $f = $c * 9/5 + 32;
+
+    echo "A temperatura em °F é: ".$f
+
+?>
---
+++ 4/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DocumenConversor de temperatura de °C para °F</title>
+</head>
+<body>
+            <form action="conversor.php" method="post">
+            <label for="">Insira a temperatura em °C</label><br>
+            <input type ="text" name = "c"><br>
+            <button type="submit">Conversão</button>
+        </form>
+</body>
+</html>
