f31b9cc4bb42597516f617c2653ab244b4500e3a
unknown <lab1065@etecjk.local>
2026-03-23 20:15:26 -0300

    Exercício 3 PHP Corrigido

--- 3/index.php
+++ 3/index.php
@@ -3,14 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>soma</title>
+    <title>Média</title>
 </head>
 <body>
-        <form action="comparar.php" method="post">
-            <input type "text" name"n1"><br>
-            <input type "text" name"n2"><br>
-            <input type "text" name"n3"><br>
-            <button type="submit">Somar</button>
-        </form>
+        <form action="media.php" method="post">
+           
+            <label for="">Nota 1</label><br>
+            <input type="text" name = "n1" id=""><br>
+            <label for="">Nota 2</label><br>
+            <input type="text" name = "n2" id=""><br>
+            <label for="">Nota 3</label><br>
+            <input type="text" name = "n3" id=""><br>
+            <input type="submit" value="Enviar">
+        </form>      
 </body>
 </html>
